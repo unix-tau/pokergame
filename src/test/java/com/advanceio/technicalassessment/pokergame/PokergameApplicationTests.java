bef6999af64b6255e6dfa029b0cfb9b0cea5e788
tau <murindataurai@gmail.com>
2024-01-23 10:31:53 +0200

    updated

--- src/test/java/com/advanceio/technicalassessment/pokergame/PokergameApplicationTests.java
+++ src/test/java/com/advanceio/technicalassessment/pokergame/PokergameApplicationTests.java
@@ -13,25 +13,10 @@ import java.util.List;
 class PokergameApplicationTests {
 
 
-    private static List<Card> generateThreeOfAKindHand() {
-        // Generating a sample Three of a Kind hand for testing
-        List<Card> threeOfAKindHand = new ArrayList<>();
-        threeOfAKindHand.add(new Card(CardSuit.HEARTS, CardRank.TWO));
-        threeOfAKindHand.add(new Card(CardSuit.DIAMONDS, CardRank.TWO));
-        threeOfAKindHand.add(new Card(CardSuit.CLUBS, CardRank.TWO));
-        threeOfAKindHand.add(new Card(CardSuit.HEARTS, CardRank.SEVEN));
-        threeOfAKindHand.add(new Card(CardSuit.SPADES, CardRank.KING));
-        return threeOfAKindHand;
-    }
 
     @Test
     void generateThreeOfAKindHand() {
         // Example usage with ThreeOfAKindRankChecker
-        List<Card> threeOfAKindHand = generateThreeOfAKindHand();
-        String threeOfAKindHandRank = evaluateHand(threeOfAKindHand, new ThreeOfAKindRankChecker());
-        System.out.println("Hand: " + threeOfAKindHand);
-        System.out.println("Hand Rank: " + threeOfAKindHandRank);
-
-    }
+         }
 
 }
