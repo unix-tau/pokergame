622090b0ae177f1078dc65c647957b049b64e687
tau <murindataurai@gmail.com>
2024-01-23 10:35:21 +0200

    updated

--- src/test/java/com/advanceio/technicalassessment/pokergame/PokergameApplicationTests.java
+++ src/test/java/com/advanceio/technicalassessment/pokergame/PokergameApplicationTests.java
@@ -1,13 +1,7 @@
 package com.advanceio.technicalassessment.pokergame;
 
-import com.advanceio.technicalassessment.pokergame.entity.Card;
-import com.advanceio.technicalassessment.pokergame.entity.CardRank;
-import com.advanceio.technicalassessment.pokergame.entity.CardSuit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootTest
 class PokergameApplicationTests {
